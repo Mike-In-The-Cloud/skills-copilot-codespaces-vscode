0497c567bc354e84482b643af4e92257bd19998b
Michael J Briggs <48106132+Mike-In-The-Cloud@users.noreply.github.com>
2023-07-31 10:29:36 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,5 @@
+// Create web server
+// $ cd /path/to/your-project-folder
+// $ python -m SimpleHTTPServer
+// Open browser to http://localhost:8000/app/
+// Open browser to http://localhost:8000/app/
